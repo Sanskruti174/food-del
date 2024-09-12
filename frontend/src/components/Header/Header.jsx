bb9b7cf33f660e3822892d6904c77e56e2610e4b
Sanskruti174 <123651040+Sanskruti174@users.noreply.github.com>
2024-09-12 17:14:06 +0530

    first-commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Everything in One place</h2>
+                <p>Your kitchen essentials await! Discover fresh produce, meats, dairy, and pantry staples in our store. From favorite brands to local delicacies, we have everything you need for delicious meals.</p>
+                <button>Shop</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
